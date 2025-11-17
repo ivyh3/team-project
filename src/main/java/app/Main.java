6cc6188fe377b68a55669b2d4d354e8eedb810cd
tessmadison <tessxmadison@gmail.com>
2025-11-17 11:42:07 -0500

    log in viewmodel

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        AppBuilder appBuilder = new AppBuilder();
+        JFrame application = appBuilder
+                .addInitialView()
+                .addLoginView()
+                .addSignupView()
+                .addLoggedInView()
+                .addSignupUseCase()
+                .addLoginUseCase()
+                .addLogoutUseCase()
+                .addChangePasswordUseCase()
+                .build();
+
+        application.pack();
+        application.setLocationRelativeTo(null);
+        application.setVisible(true);
+    }
+}
