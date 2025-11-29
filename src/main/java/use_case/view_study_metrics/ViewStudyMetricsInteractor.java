bb66d3a1fdd65ff4a648ec8bf01f0993a7fe599d
Ivy <ivyq.huang@mail.utoronto.ca>
2025-11-29 14:45:04 -0500

    interactor sonarqube fixes

--- src/main/java/use_case/view_study_metrics/ViewStudyMetricsInteractor.java
+++ src/main/java/use_case/view_study_metrics/ViewStudyMetricsInteractor.java
@@ -6,7 +6,7 @@ import entity.StudySession;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.List;
 
@@ -31,36 +31,35 @@ public class ViewStudyMetricsInteractor implements ViewStudyMetricsInputBoundary
         List<StudyQuiz> quizzes = metricsDataAccessObject.getQuizzesPerWeek(userId, week);
 
         // retrieve the daily study durations
-        Map<DayOfWeek, Duration> dailyStudyDurations = new HashMap<>();
+        Map<DayOfWeek, Duration> dailyStudyDurations = new EnumMap<>(DayOfWeek.class);
         for (StudySession session : sessions) {
             dailyStudyDurations.merge(DayOfWeek.from(session.getStartTime()), session.getDuration(), Duration::plus);
         }
         System.out.println(dailyStudyDurations); // debugging
 
         // retrieve the average quiz scores
-        Map<DayOfWeek, Float> sumMap = new HashMap<>();
-        Map<DayOfWeek, Integer> countMap = new HashMap<>();
+        Map<DayOfWeek, Float> sumMap = new EnumMap<>(DayOfWeek.class);
+        Map<DayOfWeek, Integer> countMap = new EnumMap<>(DayOfWeek.class);
 
-        for (StudyQuiz quiz : quizzes) {
+        for (StudyQuiz quiz: quizzes) {
             DayOfWeek day = quiz.getStartTime().getDayOfWeek();
             float score = quiz.getScore() * 100;
-
             sumMap.merge(day, score, Float::sum);
             countMap.merge(day, 1, Integer::sum);
         }
-        Map<DayOfWeek, Float> averageQuizScores = new HashMap<>();
-        for (DayOfWeek day : sumMap.keySet()) {
-            averageQuizScores.put(day, sumMap.get(day) / countMap.get(day));
+        Map<DayOfWeek, Float> averageQuizScores = new EnumMap<>(DayOfWeek.class);
+        for (Map.Entry<DayOfWeek, Float> entry: sumMap.entrySet()) {
+            DayOfWeek day = entry.getKey();
+            float sum = entry.getValue();
+            int count = countMap.get(day);
+            averageQuizScores.put(day, sum / count);
         }
         System.out.println(averageQuizScores); // debugging
-
-        // get the startDate for the graph
-        LocalDateTime startDate = week;
 
         ViewStudyMetricsOutputData outputData = new ViewStudyMetricsOutputData(
                 dailyStudyDurations,
                 averageQuizScores,
-                startDate
+                week
         );
 
         outputBoundary.prepareSuccessView(outputData);
